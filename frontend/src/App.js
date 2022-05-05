0fee0a9d76b37d760f9d442af197eec89ee09a01
David Kotschessa <dkotschessa@Davids-MacBook-Air.local>
2022-05-05 19:30:21 -0400

    basic updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import { Container } from "react-bootstrap";
+import Header from "./components/header";
+import Footer from "./components/footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome!</h1>
+        </Container>
+      </main>
+
+      <Footer />
     </div>
   );
 }
