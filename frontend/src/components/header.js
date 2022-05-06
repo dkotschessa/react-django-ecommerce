c45b490841bbbaebd356758472c74ec7419f6de1
David Kotschessa <dkotschessa@Davids-MacBook-Air.local>
2022-05-06 04:40:52 -0400

    added product images names and number of reviews

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 function Header() {
   return (
     <header>
@@ -9,21 +9,12 @@ function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/cart">cart</Nav.Link>
-              <Nav.Link href="/login">login</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="/cart">
+                cart<i className="fas fa-shopping-cart"> </i>
+              </Nav.Link>
+              <Nav.Link href="/login">
+                login<i className="fas fa-user"></i>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
