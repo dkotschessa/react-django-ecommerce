e9f0cb4652b458c9347c388002095f44016f72e9
David Kotschessa <dkotschessa@Davids-MacBook-Air.local>
2022-05-07 05:42:26 -0400

    added axios. whatever that is

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -1,4 +1,4 @@
-products = [
+const products = [
   {
     _id: "1",
     name: "Airpods Wireless Bluetooth Headphones",
@@ -78,3 +78,5 @@ products = [
     numReviews: 12,
   },
 ];
+
+export default products;
